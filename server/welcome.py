116dd9260447ab7513ce5d6faa40895f35762a71
Chita <zhouzhida_1997@outlook.com>
2020-08-05 22:59:35 +0800

    单词相关API完成

--- server/welcome.py
+++ server/welcome.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, send_file, request, session
 from flask_cors import CORS
+import sys
 import json
 import os
 import ssl as ssl_lib
@@ -24,6 +25,16 @@ class group_obj:
         self.add_datetime = add_datetime
         self.upd_datetime = upd_datetime
 
+# 单词数据对象类，用于快速映射生成json对象
+class word_obj:
+    def __init__(self, words_id, group_id, words_jp, trans_cn, add_datetime, upd_datetime):
+        self.words_id = words_id
+        self.group_id = group_id
+        self.words_jp = words_jp
+        self.trans_cn = trans_cn
+        self.add_datetime = add_datetime
+        self.upd_datetime = upd_datetime
+
 # 工具类：时间戳转换日期文本
 def util_timestamp_2_date(timestamp):
     timeArray = time.localtime(int(timestamp))
@@ -45,7 +56,7 @@ def add_group():
             return "parameter_missing"
         else:
             current_time = int(time.time())
-            sql = 'insert into "main"."group"("group_id", "group_name", "add_datetime", "upd_datetime") VALUES (NULL, \''+ para_json_obj["group_name"] +'\', '+ str(current_time) +', '+ str(current_time) +')'
+            sql = 'insert into "main"."group"("group_id", "group_name", "add_datetime", "upd_datetime") VALUES (NULL, \'' + str(para_json_obj["group_name"]) +'\', ' + str(current_time) +', ' + str(current_time) +')'
             conn = sqlite3.connect('data/core.db')
             c = conn.cursor()
             c.execute(sql)
@@ -64,6 +75,65 @@ def get_group():
     for row in cursor:
         data_groups.append(json.loads(json.dumps(group_obj(row[0], row[1], util_timestamp_2_date(row[2]), util_timestamp_2_date(row[3])).__dict__, ensure_ascii=True)))
     return json.dumps(data_groups)
+
+# 添加单词
+# 参数(json)：
+# 1. words_jp (日语单词,必须)
+# 2. trans_cn (中文意思,必须)
+# 3. group_id (所在分组ID,必须)
+@app.route("/word", methods=['POST'])
+def add_word():
+    if request.get_data(as_text=True) != '':
+        para_json_obj = json.loads(request.get_data(as_text=True))
+        if ("words_jp" not in para_json_obj or "trans_cn" not in para_json_obj or "group_id" not in para_json_obj):
+            return "parameter_missing"
+        else:
+            current_time = int(time.time())
+            sql = 'insert into "main"."words"("words_id", "group_id", "words_jp", "trans_cn", "add_datetime", "upd_datetime") VALUES (NULL, \'' + str(para_json_obj["group_id"]) +'\', \'' + str(para_json_obj["words_jp"]) +'\', \'' + str(para_json_obj["trans_cn"]) + '\', '  + str(current_time) +', ' + str(current_time) +')'
+            conn = sqlite3.connect('data/core.db')
+            c = conn.cursor()
+            c.execute(sql)
+            conn.commit()
+            conn.close()
+            return "add_success"
+
+# 获取全部单词
+@app.route("/word", methods=['GET'])
+def get_word():
+    sql = 'SELECT words_id, group_id, words_jp, trans_cn, add_datetime, upd_datetime FROM "words"'
+    conn = sqlite3.connect('data/core.db')
+    c = conn.cursor()
+    cursor = c.execute(sql)
+    data_words = []
+    for row in cursor:
+        data_words.append(json.loads(json.dumps(word_obj(row[0], row[1], row[2], row[3], util_timestamp_2_date(row[4]), util_timestamp_2_date(row[5])).__dict__, ensure_ascii=True)))
+    return json.dumps(data_words)
+
+# 删除单词
+# 参数(json)：
+# 1. words_id (单词ID,必须)
+@app.route("/word", methods=['DELETE'])
+def del_word():
+    if request.get_data(as_text=True) != '':
+        para_json_obj = json.loads(request.get_data(as_text=True))
+        if ("words_id" not in para_json_obj):
+            return "parameter_missing"
+        else:
+            sql = 'DELETE FROM "words" WHERE words_id = '+str(para_json_obj["words_id"])
+            conn = sqlite3.connect('data/core.db')
+            c = conn.cursor()
+            try:
+                cursor = c.execute(sql)
+                conn.commit()
+                if conn.total_changes != 0:
+                    return "del_success"
+                else:
+                    return "del_error"
+            except:
+                return "del error:" + sys.exc_info()[0]
+            finally:
+                conn.close()
+            
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(24)
