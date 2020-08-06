580d7ca2a383e62d526fe5abdff247cd445501cb
Chita <zhouzhida_1997@outlook.com>
2020-08-07 00:29:10 +0800

    后端API初步开发完成

--- server/welcome.py
+++ server/welcome.py
@@ -35,6 +35,13 @@ class word_obj:
         self.add_datetime = add_datetime
         self.upd_datetime = upd_datetime
 
+# 考试结果对象类，用于快速映射生成json对象
+class exam_result_obj:
+    def __init__(self, test_id, score, finish_datetime):
+        self.test_id = test_id
+        self.score = score
+        self.finish_datetime = finish_datetime
+
 # 工具类：时间戳转换日期文本
 def util_timestamp_2_date(timestamp):
     timeArray = time.localtime(int(timestamp))
@@ -130,10 +137,131 @@ def del_word():
                 else:
                     return "del_error"
             except:
-                return "del error:" + sys.exc_info()[0]
+                return "del error:" + str(sys.exc_info()[0])
             finally:
                 conn.close()
             
+# 创建考试
+# 参数(get url)：
+# 1. words_group (单词分组,必须)
+# 2. type (考试种类,必须,汉译日-1；日译汉-2)
+@app.route("/exam", methods=['GET'])
+def create_exam():
+    group = request.args.get("words_group")
+    exam_type = request.args.get("type")
+    # 为空或缺少参数判断
+    if group == None or exam_type == None or group == '' or exam_type == '':
+        return "parameter_error"
+    # 考试种类判断
+    if exam_type != '1' and exam_type != '2':
+        return "exam_type_error"
+    conn = sqlite3.connect('data/core.db')
+    c = conn.cursor()
+    # 单词分组判断
+    sql_group_exist = 'SELECT group_id FROM "group" WHERE group_id= '+str(group)
+    cursor = c.execute(sql_group_exist)
+    list_group_exist = cursor.fetchall()
+    if len(list_group_exist) > 0:
+        # 从词汇表中根据指定单词分组随机抽取20条数据（若不足20条则返回随机乱序的全部记录）
+        sql_get_words = 'SELECT words_id, group_id, words_jp, trans_cn, add_datetime, upd_datetime FROM words WHERE group_id= '+ str(group) +' ORDER BY RANDOM() limit 20;'
+        cursor = c.execute(sql_get_words)
+        list_exam_words = cursor.fetchall()
+        if len(list_exam_words) > 0:
+            record_id = 1
+            # 创建考试ID，使用当前时间戳作为考试ID
+            exam_id = int(time.time())
+            # 将考试ID和词汇列表封装，用于返回给前台
+            data_words = []
+            for row in list_exam_words:
+                data_words.append(json.loads(json.dumps(word_obj(row[0], row[1], row[2], row[3], util_timestamp_2_date(row[4]), util_timestamp_2_date(row[5])).__dict__, ensure_ascii=True)))
+                # 将随机抽取的词汇ID和其他考试信息写入到考试记录表中
+                sql_test_record = 'INSERT INTO test_record (test_id, record_id, "type", words_id, answer, add_datetime, ans_datetime) VALUES('
+                sql_test_record = sql_test_record + str(exam_id) + ', '
+                sql_test_record = sql_test_record + str(record_id) + ', '
+                sql_test_record = sql_test_record + str(exam_type) + ', '
+                sql_test_record = sql_test_record + str(row[0]) + ', '
+                sql_test_record = sql_test_record + 'NULL' + ', '
+                sql_test_record = sql_test_record + str(exam_id) + ', '
+                sql_test_record = sql_test_record + 'NULL' + ')'
+                c.execute(sql_test_record)     
+                record_id += 1
+            conn.commit()
+            conn.close()
+            exam_detail_info = {}
+            exam_detail_info['exam_id'] = exam_id
+            exam_detail_info['exam_words'] = data_words
+            return json.dumps(exam_detail_info)
+        else:
+            # 当前分组下没有词汇
+            conn.close()
+            return "group_has_no_words"
+    else:
+        # 词汇分组不存在
+        conn.close()
+        return "words_group_error"
+
+# 提交小题
+# 参数(json)
+# 1. test_id (考试ID,必须)
+# 2. words_id (单词ID，必须)
+# 3. answer (答案，必须)
+@app.route("/exam", methods=['POST'])
+def upd_answer():
+    if request.get_data(as_text=True) != '':
+        para_json_obj = json.loads(request.get_data(as_text=True))
+        if ("test_id" not in para_json_obj or "words_id" not in para_json_obj or "answer" not in para_json_obj):
+            return "parameter_missing"
+        else:
+            ans_time = int(time.time())
+            sql_upd = 'UPDATE "main"."test_record" SET "answer" = \'' + str(para_json_obj["answer"]) + '\', "ans_datetime" = '+ str(ans_time) +' WHERE "test_id" = '+ str(para_json_obj["test_id"]) +' AND "words_id" = '+ str(para_json_obj["words_id"])
+            conn = sqlite3.connect('data/core.db')
+            c = conn.cursor()
+            try:
+                cursor = c.execute(sql_upd)
+                conn.commit()
+                if conn.total_changes != 0:
+                    return "ans_success"
+                else:
+                    return "ans_error"
+            except:
+                return "ans error:" + str(sys.exc_info()[0])
+            finally:
+                conn.close()
+
+# 提交考试，打分记录成绩
+# 参数(json)
+# 1. test_id (考试ID,必须)
+@app.route("/exam_result", methods=['POST'])
+def com_exam():
+    if request.get_data(as_text=True) != '':
+        para_json_obj = json.loads(request.get_data(as_text=True))
+        if ("test_id" not in para_json_obj):
+            return "parameter_missing"
+        else:
+            com_time = int(time.time())
+            sql_get_points = 'SELECT (tr.answer = w.words_jp)*5 AS points FROM "test_record" tr INNER JOIN "words" w ON tr.words_id = w.words_id WHERE tr.test_id = '+ str(para_json_obj["test_id"])
+            conn = sqlite3.connect('data/core.db')
+            c = conn.cursor()
+            try:
+                cursor = c.execute(sql_get_points)
+                list_points = cursor.fetchall()
+                total_points = 0
+                if len(list_points) > 0:
+                    for row in list_points:
+                        total_points = total_points + int(row[0])
+                    sql_ins_test_result = 'INSERT INTO "main"."test_result"("test_id", "score", "finish_datetime") VALUES ('+ str(para_json_obj["test_id"]) +', '+ str(total_points) +', '+ str(com_time) +')'
+                    cursor = c.execute(sql_ins_test_result)
+                    conn.commit()
+                    if conn.total_changes != 0:
+                        return json.dumps(exam_result_obj(str(para_json_obj["test_id"]), total_points, util_timestamp_2_date(com_time)).__dict__, ensure_ascii=True)
+                    else:
+                        return "write result error"
+                else:
+                    return "test_id_incorrect"
+            except:
+                return "get result error:" + str(sys.exc_info()[0])
+            finally:
+                conn.close()
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(24)
